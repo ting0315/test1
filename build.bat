5df2476e374e001ceb72fe701cb68abbf99942e3
ting0315 <1096314138@qq.com>
2019-10-29 17:10:11 +0800

    update

---
+++ build.bat
@@ -0,0 +1,4 @@
+md build 
+bd build
+cmake -G "Visual Studio 15 2017" ../snake
+cmake --build .
---
+++ snake/snake.cpp
@@ -0,0 +1,26 @@
+// snake.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include<windows.h>
+void Init()
+{
+}
+void GameLoop()
+{
+	while(1)
+	{
+		Sleep(100);
+	}
+}
+void Score()
+{
+}
+int main(int argc, char* argv[])
+{
+	Init();
+	GameLoop();
+	Score();
+	return 0;
+}
+
