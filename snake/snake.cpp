248e8744b3089926d922514632bef03b3ddb35e4
ting0315 <1096314138@qq.com>
2019-10-31 11:03:06 +0800

    吃食物

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -99,6 +99,58 @@ void UpdateScreen()
 {
 	DrawSnake();
 }
+void SnakeMove(int key)
+{
+	int delta_x=0;
+	int delta_y=0;
+	if(key=='w'||key=='W')
+	{
+		delta_x=0;
+	    delta_y=-1;
+	}
+	else if(key=='s'||key=='S')
+	{
+		delta_x=0;
+	    delta_y=1;
+	}
+	else if(key=='a'||key=='A')
+	{
+		delta_x=-1;
+	    delta_y=0;
+	}
+	else if(key=='d'||key=='D')
+	{
+		delta_x=1;
+	    delta_y=0;
+	}
+	else
+	{
+		return;
+	}
+	DrawChar(g_snake.pos[g_snake.size -1].x,g_snake.pos[g_snake.size-1].y,' ');
+	for (int i= g_snake.size -1;i>0;i--)
+	{
+		g_snake.pos[i].x = g_snake.pos[i-1].x;
+		g_snake.pos[i].y = g_snake.pos[i-1].y;
+	}
+	g_snake.pos[0].x+=delta_x;
+	g_snake.pos[0].y+=delta_y;
+}
+
+void EatFood()
+{
+	if(g_snake.pos[0].x==g_food.x &&
+	   g_snake.pos[0].y==g_food.y)
+	   {
+		g_snake.size++;
+     	g_snake.pos[g_snake.size-1].x=g_food.x;
+		g_snake.pos[g_snake.size-1].y=g_food.y;
+		InitFood();
+	}
+}
+
+
+
 void GameLoop()
 {
 	int key =0;
@@ -113,6 +165,8 @@ void GameLoop()
 		{
 			return;
 		}
+		SnakeMove(key);
+		EatFood();
 		UpdateScreen();
 		Sleep(100);
 	}
